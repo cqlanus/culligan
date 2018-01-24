0b163336ea3e4754aac91dfbeaf639ae065e59bb
Chris Lanus <chris.lanus@isobar.com>
2018-01-24 09:34:14 -0600

    responding to iprospect mobile feedback

--- contact-us-form.js
+++ contact-us-form.js
@@ -1,19 +1,16 @@
 $(document).ready(function() {
-    var emailLabel = window.location.pathname.substring(1,3) === 'fr' ? 'Envoyer un email' : 'Email Me';
-    var callLabel = window.location.pathname.substring(1,3) === 'fr' ? 'Appelle-moi' : 'Call Me'
-    var securityLabel = window.location.pathname.substring(1,3) === 'fr' ? 'Entrer le code de sécurité:' : 'Enter security code:'
-    
-    $('.contact-us-form-content label').first().text(emailLabel).addClass('active-label');
-    $('.contact-us-form-content label').first().next().text(callLabel);
-    $('.captcha-field-1 label').text(securityLabel);
+    setFormLabels();
+    setFormPlaceholders();
+    setRequiredFields();
     
     $('.phone-field').hide();
+    $('.email-field').addClass('active-tabber');
 
     $('.contact-us-form-content label').on('click', function() {
         if (!$(this).hasClass('active-label')) {
-            $('.active-tabber').removeClass('active-tabber');
+            $('.active-tabber').removeClass('active-tabber').prop('required', false);
             $('.active-label').removeClass('active-label');
-            $('#' + $(this).attr('for')).addClass('active-tabber');
+            $('#' + $(this).attr('for')).addClass('active-tabber').prop('required', true);
             $(this).addClass('active-label');
         }
     });
@@ -34,9 +31,11 @@ $(document).ready(function() {
     });
 
     $(document).on('click', '.contact-us-form-content input[type="submit"]', function() {
-        $(this).click(function() { return false; });
-        $(this).addClass('disabled');
-        return true;
+        if (requiredFieldsFilled()) {
+            $(this).click(function() { return false; })
+            $(this).addClass('disabled');
+            return true;
+        } 
     });
 
     // var sticky = new Waypoint.Sticky({
@@ -55,3 +54,40 @@ $(document).ready(function() {
     });
 
 });
+
+
+function requiredFieldsFilled() {
+    var hasName = !!$('.name-field').val();
+    var hasZip = !!$('.zip-field').val();
+    var hasContact = (!!$('.active-tabber.email-field').val() || !!$('.active-tabber.phone-field').val())
+    return hasName && hasZip && hasContact
+}
+
+
+function setFormLabels() {
+    var emailLabel = window.location.pathname.substring(1, 3) === 'fr' ? 'Envoyer un courriel' : 'Email Me';
+    var callLabel = window.location.pathname.substring(1, 3) === 'fr' ? 'Appelle-moi' : 'Call Me'
+    var securityLabel = window.location.pathname.substring(1, 3) === 'fr' ? 'Entrer le code de sécurité:' : 'Enter security code:'
+
+    $('.contact-us-form-content label').first().text(emailLabel).addClass('active-label');
+    $('.contact-us-form-content label').first().next().text(callLabel);
+    $('.captcha-field-1 label').text(securityLabel);
+}
+
+function setFormPlaceholders() {
+    var namePlaceholder = window.location.pathname.substring(1, 3) === 'fr' ? 'Prénom' : 'Full Name';
+    var zipPlaceholder = window.location.pathname.substring(1, 3) === 'fr' ? 'Code Postal' : 'Zip Code';
+    var emailPlaceholder = window.location.pathname.substring(1, 3) === 'fr' ? 'Adresse De Courriel' : 'Email Address';
+    var phonePlaceholder = window.location.pathname.substring(1, 3) === 'fr' ? 'No. De Téléphone' : 'Phone';
+
+    $('.name-field').attr('placeholder', namePlaceholder);
+    $('.zip-field').attr('placeholder', zipPlaceholder);
+    $('.email-field').attr('placeholder', emailPlaceholder);
+    $('.phone-field').attr('placeholder', phonePlaceholder);
+}
+
+function setRequiredFields() {
+    $('.name-field').prop('required', true);
+    $('.zip-field').prop('required', true);
+    $('.email-field').prop('required', true);
+}
--- hey_culligan/home-form.js
+++ hey_culligan/home-form.js
@@ -1,88 +1,129 @@
-  $(document).ready(function(){
-    var emailLabel = window.location.pathname.substring(1,3) === 'fr' ? 'Envoyer un email' : 'Email Me';
-    var callLabel = window.location.pathname.substring(1,3) === 'fr' ? 'Appelle-moi' : 'Call Me'
-    var securityLabel = window.location.pathname.substring(1,3) === 'fr' ? 'Entrer le code de sécurité:' : 'Enter security code:'
-    
-    $('.aside-form .form-wrapper label').first().text(emailLabel).addClass('active-label');
-    $('.aside-form .form-wrapper label').first().next().text(callLabel);
-    $('.captcha-field-1 label').text(securityLabel);
-    
-    $('.phone-field').hide()
-      $('.aside-form .form-wrapper label').on('click', function(){
-        if(!$(this).hasClass('active-label')){
-          $('.active-tabber').removeClass('active-tabber');
-          $('.active-label').removeClass('active-label');
-          $('#'+ $(this).attr('for')).addClass('active-tabber');
-          $(this).addClass('active-label');
-        }
-      });
-      $('#form-toggle').on('click', function(){
-        $('.sticky-form.stuck').toggleClass('active');
-      });
+$(document).ready(function() {
 
-      $('.email-field-label').click(function() {
+    setFormLabels();
+    setFormPlaceholders();
+    setRequiredFields();
+
+    $('.phone-field').hide()
+    $('.email-field').addClass('active-tabber');
+    $('.aside-form .form-wrapper label').on('click', function() {
+        if (!$(this).hasClass('active-label')) {
+            var inputSelector = '#' + $(this).attr('for');
+            console.log(inputSelector);
+            $('.active-tabber').removeClass('active-tabber').prop('required', false);
+            $('.active-label').removeClass('active-label');
+            $(inputSelector).addClass('active-tabber').prop('required', true);
+            $(this).addClass('active-label');
+        }
+    });
+    $('#form-toggle').on('click', function() {
+        $('.sticky-form.stuck').toggleClass('active');
+    });
+
+    $('.email-field-label').click(function() {
         $('.phone-field').hide();
         $('.email-field').show();
-      });
+    });
 
-      $('.phone-field-label').click(function() {
+    $('.phone-field-label').click(function() {
         $('.email-field').hide();
         $('.phone-field').show();
-      });
+    });
 
-      var sticky;
-      var inview;
-      function createSticky(){
+    var sticky;
+    var inview;
+
+    function createSticky() {
         // var isDesktop = $(window).width() > 768;
         var inviewElement = /*isDesktop ?*/ $('.gif-feature-text h3')[0] /*: $('.form-clear-el')[0]*/
 
-        if($(window).height() > ($('.sticky-form').height())){
-          sticky = new Waypoint.Sticky({
-            element: $('.sticky-form')[0]
-          });
-          setTimeout(function() {
-            inview = new Waypoint.Inview({
-              element: inviewElement,
-              enter: function(direction) {
-                /*isDesktop && */direction === 'up' &&  $('.sticky-form.stuck .form-wrapper').css('display', 'inherit')
-              },
-              exited: function(direction) {
-                console.log(inviewElement);
-                  /*isDesktop && */direction === 'down' && $('.sticky-form.stuck .form-wrapper').css('display', 'none');
-              },
-            })
-          }, 1000);
+        if ($(window).height() > ($('.sticky-form').height())) {
+            sticky = new Waypoint.Sticky({
+                element: $('.sticky-form')[0]
+            });
+            setTimeout(function() {
+                inview = new Waypoint.Inview({
+                    element: inviewElement,
+                    enter: function(direction) {
+                        /*isDesktop && */
+                        direction === 'up' && $('.sticky-form.stuck .form-wrapper').css('display', 'inherit')
+                    },
+                    exited: function(direction) {
+                        console.log(inviewElement);
+                        /*isDesktop && */
+                        direction === 'down' && $('.sticky-form.stuck .form-wrapper').css('display', 'none');
+                    },
+                })
+            }, 1000);
         }
         resizeSticky();
-      }
-      function initStickies(){
-          createSticky();
-      }
-      function resizeSticky(){
+    }
+
+    function initStickies() {
+        createSticky();
+    }
+
+    function resizeSticky() {
         if (Modernizr.mq('(min-width: 1024px)')) {
-          // $('.sticky-form').width(($('.container').outerWidth() * 0.25 - 30));
-          $('.sticky-form').width(265);
+            // $('.sticky-form').width(($('.container').outerWidth() * 0.25 - 30));
+            $('.sticky-form').width(265);
+        } else if (Modernizr.mq('(min-width: 768px)') && Modernizr.mq('(max-width: 1024px)')) {
+            // $('.sticky-form').width(($('.zoneMainContent').width() * 0.3333333 - 30));
+            $('.sticky-form').width(225);
+        } else {
+            $('.sticky-form').width($('.zoneMainContent').width());
         }
-        else if(Modernizr.mq('(min-width: 768px)') && Modernizr.mq('(max-width: 1024px)')) {
-          // $('.sticky-form').width(($('.zoneMainContent').width() * 0.3333333 - 30));
-          $('.sticky-form').width(225);
-        }
-        else{
-          $('.sticky-form').width($('.zoneMainContent').width());
-        }
-      }
-      $(window).resize(function(){
+    }
+    $(window).resize(function() {
         resizeSticky();
-      });
-      initStickies();
-
-
-
-      $(document).on('click', '.form-wrapper input[type="submit"]', function() {
-        $(this).click(function() { return false; })
-        $(this).addClass('disabled');
-        return true;
-      })
-
-
     });
+    initStickies();
+
+
+
+    $(document).on('click', '.form-wrapper input[type="submit"]', function() {
+        if (requiredFieldsFilled()) {
+            $(this).click(function() { return false; })
+            $(this).addClass('disabled');
+            return true;
+        } 
+    })
+
+
+});
+
+function requiredFieldsFilled() {
+    var hasName = !!$('.name-field').val();
+    var hasZip = !!$('.zip-field').val();
+    var hasContact = (!!$('.active-tabber.email-field').val() || !!$('.active-tabber.phone-field').val())
+    return hasName && hasZip && hasContact
+}
+
+
+function setFormLabels() {
+    var emailLabel = window.location.pathname.substring(1, 3) === 'fr' ? 'Envoyer un courriel' : 'Email Me';
+    var callLabel = window.location.pathname.substring(1, 3) === 'fr' ? 'Appelle-moi' : 'Call Me'
+    var securityLabel = window.location.pathname.substring(1, 3) === 'fr' ? 'Entrer le code de sécurité:' : 'Enter security code:'
+
+    $('.aside-form .form-wrapper label').first().text(emailLabel).addClass('active-label');
+    $('.aside-form .form-wrapper label').first().next().text(callLabel);
+    $('.captcha-field-1 label').text(securityLabel);
+}
+
+function setFormPlaceholders() {
+    var namePlaceholder = window.location.pathname.substring(1, 3) === 'fr' ? 'Prénom' : 'Full Name';
+    var zipPlaceholder = window.location.pathname.substring(1, 3) === 'fr' ? 'Code Postal' : 'Zip Code';
+    var emailPlaceholder = window.location.pathname.substring(1, 3) === 'fr' ? 'Adresse De Courriel' : 'Email Address';
+    var phonePlaceholder = window.location.pathname.substring(1, 3) === 'fr' ? 'No. De Téléphone' : 'Phone';
+
+    $('.name-field').attr('placeholder', namePlaceholder);
+    $('.zip-field').attr('placeholder', zipPlaceholder);
+    $('.email-field').attr('placeholder', emailPlaceholder);
+    $('.phone-field').attr('placeholder', phonePlaceholder);
+}
+
+function setRequiredFields() {
+    $('.name-field').prop('required', true);
+    $('.zip-field').prop('required', true);
+    $('.email-field').prop('required', true);
+}
